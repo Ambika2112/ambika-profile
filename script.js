272118097e06ea7d3ddacda12d39aaed23baa2e1
Ambika2112 <ambikarajputh@outlook.com>
2025-06-02 19:17:50 +0100

    Animated

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function toggleSection(id) {
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(sec => sec.classList.remove('active'));
+    document.querySelectorAll('.content-section').forEach(sec => sec.classList.remove('active'));
     document.getElementById(id).classList.add('active');
 }
